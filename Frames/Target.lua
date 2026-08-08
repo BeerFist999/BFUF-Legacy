63e94b5d5c2e41ae4812d45e58b58d94bf81dfee
BeerFist999 <znet9999@gmail.com>
2026-08-08 14:17:42 +0300

    fix(target): support raid creature portraits

--- Frames/Target.lua
+++ Frames/Target.lua
@@ -366,7 +366,10 @@ function Target:Create()
     root:RegisterEvent("UNIT_NAME_UPDATE")
     root:RegisterEvent("UNIT_PORTRAIT_UPDATE")
     root:RegisterEvent("UNIT_MODEL_CHANGED")
+    root:RegisterEvent("UNIT_CONNECTION")
     root:RegisterEvent("PORTRAITS_UPDATED")
+    root:RegisterEvent("PARTY_MEMBER_ENABLE")
+    root:RegisterEvent("PARTY_MEMBER_DISABLE")
     root:SetScript("OnEvent", function(_, event, unit)
         if event == "PLAYER_REGEN_ENABLED" and root.layoutPending then
             Target:UpdateLayout(root)
@@ -384,7 +387,9 @@ function Target:Create()
             return
         end
 
-        if event == "PORTRAITS_UPDATED" then
+        if event == "PORTRAITS_UPDATED"
+            or event == "PARTY_MEMBER_ENABLE"
+            or event == "PARTY_MEMBER_DISABLE" then
             Target:UpdatePortrait(root)
             return
         end
